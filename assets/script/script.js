5840c92b2ac3adc4bfe1b0ef8a3c360f282b4f70
Glazunova Tomara <glazunovatomara5@gmail.com>
2024-08-22 23:45:52 +0700

    fix:js for main__gallery_text

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,8 +3,9 @@ const rub = 'рублей';
 let price = 500;
 const calculateTotalPrice = (quantity = 2,price = 500) => {
     const result = quantity * price;
-    let showMessage = `Стоимость покупки: ${result} ${rub}`;
-    alert(showMessage)
+    let showMessage = `Стоимость покупки: ${result.toLocaleString('ru-RU')} ${rub}`;
+    let paragraph = document.getElementById ('price');
+    paragraph.textContent = showMessage ;
 }
 calculateTotalPrice()
 //Вам необходимо создать функцию calculateTotalPrice(), которая принимает два аргумента - quantity (количество товаров) и price (цена за единицу товара). Если значения не переданы, функция должна использовать значения по умолчанию, например: quantity = 2 и price = 15000000.
